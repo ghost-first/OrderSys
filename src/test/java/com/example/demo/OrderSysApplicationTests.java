a9b900341574c065bbe8d011b2a2570d21aeae02
YUPuff <362834277@qq.com>
2022-02-23 14:07:57 +0800

    逆向工程

--- src/test/java/com/example/demo/OrderSysApplicationTests.java
+++ src/test/java/com/example/demo/OrderSysApplicationTests.java
@@ -1,7 +1,17 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class OrderSysApplicationTests {
@@ -9,5 +19,15 @@ class OrderSysApplicationTests {
     @Test
     void contextLoads() {
     }
-
+    @Test
+    void testMBG() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("D:\\project3\\github\\OrderSys\\src\\main\\resources\\generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
 }
