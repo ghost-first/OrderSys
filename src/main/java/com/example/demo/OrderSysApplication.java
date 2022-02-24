db1e495e003f06e056cf3d78843c494ad5185e48
LiManyuan663 <gtlmy5126@outlook.com>
2022-02-24 11:36:09 +0800

    上传菜品图片，可能需要和前端结合一下再测试

--- src/main/java/com/example/demo/OrderSysApplication.java
+++ src/main/java/com/example/demo/OrderSysApplication.java
@@ -10,5 +10,4 @@ public class OrderSysApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderSysApplication.class, args);
     }
-
 }
--- src/main/java/com/example/demo/controller/DishesController.java
+++ src/main/java/com/example/demo/controller/DishesController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class DishesController {
@@ -34,7 +39,7 @@ public class DishesController {
     @ResponseBody
     @RequestMapping("querydish")
     public Dishes queryDish(int dishid){
-        System.out.println("开始queryDish");
+//        System.out.println("开始queryDish");
         Dishes dish = dishesService.findByDid(dishid);
         System.out.println(dish);
 //        Dishes test = new Dishes();
@@ -54,8 +59,39 @@ public class DishesController {
 
     @ResponseBody
     @RequestMapping("adddish")
-    public boolean addDish(Dishes dish){
+    public boolean addDish(Dishes dish,MultipartHttpServletRequest request)throws Exception {
+        System.out.println(dish);
+//        dish.setDishPic(uploadPic("dish_pic",request));  //设置图片
         return dishesService.addDishes(dish);
     }
 
+    @ResponseBody
+    @RequestMapping("editdish")
+    public boolean editDish(Dishes dish,MultipartHttpServletRequest request)throws Exception {
+//        dish.setDishPic(uploadPic("dish_pic",request));  //设置图片
+        System.out.println(dish);
+        return dishesService.editDishes(dish);
+    }
+
+    //图片上传
+    public String uploadPic(String itemName, MultipartHttpServletRequest request)throws Exception {
+        String pathRoot = request.getSession().getServletContext().getRealPath(""); //获得服务器上绝对路径
+        String path = "";// 保存的图片名
+        List<String> paths = new ArrayList<String>();//存放多个图片名
+        for(MultipartFile mf : request.getFiles(itemName)) {
+            if(!mf.isEmpty()) {
+                String uuid = UUID.randomUUID().toString().replaceAll("-", "");//获得一个不会重复的文件名
+                String contentType = mf.getContentType();//获得图片的后缀名
+                String suffixName = contentType.substring(contentType.indexOf("/")+1);//截取图片后缀名，它是一个特殊的格式，比如XXXX/JPEG
+                if(suffixName.equals("octet-stream"))return null;//一种特殊情况，一般不做处理
+                path = "/files/"+uuid+"."+suffixName;//合成图片的路径。files是文件夹的名字
+                mf.transferTo(new File(pathRoot+path));//存到服务器上
+                paths.add(path);
+            }
+        }
+        if(paths.size()!=0)
+            return String.join(";", paths);
+        else
+            return null;
+    }
 }
