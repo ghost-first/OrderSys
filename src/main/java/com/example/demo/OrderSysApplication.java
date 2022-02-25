e6ee1892406d921fb61f87a0592bf4f5c6155c71
LiManyuan663 <gtlmy5126@outlook.com>
2022-02-25 14:15:08 +0800

    进行了一些打包的配置

--- src/main/java/com/example/demo/OrderSysApplication.java
+++ src/main/java/com/example/demo/OrderSysApplication.java
@@ -3,12 +3,33 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @MapperScan("com.example.demo")
-public class OrderSysApplication {
+public class OrderSysApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(OrderSysApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(OrderSysApplication.class, args);
     }
 }
+
+/**
+ * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+ */
+//public class SpringBootStartApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // 注意这里要指向原先用main方法执行的Application启动类
+//        return builder.sources(OrderSysApplication.class);
+//    }
+//}
