13f6f8ef3ed9d050410a4b7c6b719db70d7b4803
LiManyuan663 <gtlmy5126@outlook.com>
2022-02-25 15:41:10 +0800

    merge2.1

--- src/main/java/com/example/demo/controller/DishOrderController.java
+++ src/main/java/com/example/demo/controller/DishOrderController.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.DishOrder;
 import com.example.demo.service.DishOrderService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,8 +22,18 @@ public class DishOrderController {
 //        return dishOrderService.findSomeDishOrder(dishOrder);
 //    }
     @RequestMapping("/querySome")
-    public List<Map<String,Object>> findSomeDishOrder(Integer tableId){
-        return dishOrderService.findSomeDishOrder(tableId);
+    public List<Map<String,Object>> findSomeDishOrder(@Param("dishName")String dishName,
+                                                      @Param("dishState")Integer dishState,
+                                                      @Param("tableId")Integer tableId,
+                                                      @Param("count")Integer count){
+        Map<String,Object> map = new HashMap<>();
+        map.put("dishName",dishName);
+        map.put("dishState",dishState);
+        map.put("tableId",tableId);
+        map.put("count",count);
+        List<Map<String,Object>> rs = dishOrderService.findSomeDishOrder(map);
+        System.out.println(rs.size());
+        return dishOrderService.findSomeDishOrder(map);
     }
     @RequestMapping("/update")
     public DishOrder updateDishOrder(DishOrder dishOrder){
